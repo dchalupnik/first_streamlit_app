5567096bc436aacbf03d416a172021e70c0b68d8
Damian Chałupnik <d.chalupnik@hotmail.com>
2024-01-28 23:16:32 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,10 @@ streamlit.title('My Parents New Healthy Dinner')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bllueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocker Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣Omega 3 & Bllueberry Oatmeal')
+streamlit.text('🥗Kale, Spinach & Rocker Smoothie')
+streamlit.text('🐔Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
