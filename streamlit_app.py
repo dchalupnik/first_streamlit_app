4ff5626aec8faef1d2757a2675194e20e33e4c83
Damian Chałupnik <d.chalupnik@hotmail.com>
2024-01-29 19:12:09 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,7 @@ streamlit.dataframe(fruits_to_show )
 streamlit.header("Fruityvice Fruit Advice!")
 try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  streamlit.write(fruit_choice)
+  streamlit.write(f"+{fruit_choice}+")
   if fruit_choice and fruit_choice > '':
       fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
       fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
